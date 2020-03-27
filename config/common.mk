d24afee5d732fe45099b1a0c8730c13e4cae948c
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-27 16:55:56 -0300

    swift: Port face unlock feature
    Change-Id: I9d4bb7174d28ca067665893a8096b3176a5aa7a6

--- config/common.mk
+++ config/common.mk
@@ -102,6 +102,16 @@ sdclang_already_warned := true
 endif
 else
 
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := false
+ifneq ($(TARGET_DISABLE_ALTERNATIVE_FACE_UNLOCK), true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+TARGET_FACE_UNLOCK_SUPPORTED := true
+endif
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
+
 # include definitions for SDCLANG
 include vendor/swift/sdclang/sdclang.mk
 endif
