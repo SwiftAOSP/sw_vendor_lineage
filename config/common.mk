d4ee9f31b472b766a1b15c8997d1663aeb3fe901
Alessandro Astone <ales.astone@gmail.com>
2020-12-28 12:34:21 +0100

    config: More checks for copying AB backuptool scripts
    Pixels tree have moved AB_OTA_PARTITIONS to BoardConfig.mk,
    which is unavailable when parsing config/common.mk
    In order to still copy AB backuptool scripts for them, check for
    AB_OTA_POSTINSTALL_CONFIG too which still lives in device.mk
    
    Change-Id: I871e2595bc4f083db6491de0d9574bae2794e654

--- config/common.mk
+++ config/common.mk
@@ -38,7 +38,7 @@ PRODUCT_COPY_FILES += \
     vendor/swift/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
     vendor/swift/prebuilt/common/bin/50-swift.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-swift.sh
 
-ifneq ($(AB_OTA_PARTITIONS),)
+ifneq ($(strip $(AB_OTA_PARTITIONS) $(AB_OTA_POSTINSTALL_CONFIG)),)
 PRODUCT_COPY_FILES += \
     vendor/swift/prebuilt/common/bin/backuptool_ab.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_ab.sh \
     vendor/swift/prebuilt/common/bin/backuptool_ab.functions:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_ab.functions \
