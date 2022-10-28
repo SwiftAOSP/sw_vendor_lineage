ConquerOS/vendor_conquer

vendor: import sdclang configs

config/common.mk
@@ -105,6 +105,19 @@ PRODUCT_COPY_FILES += \
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/conquer/overlay/dictionaries
 
+ifneq ($(HOST_OS),linux)
+ifneq ($(sdclang_already_warned),true)
+$(warning **********************************************)
+$(warning * SDCLANG is not supported on non-linux hosts.)
+$(warning **********************************************)
+sdclang_already_warned := true
+endif
+else
+
+# include definitions for SDCLANG
+include vendor/conquer/sdclang/sdclang.mk
+endif
+
 # Include AOSP audio files
 include vendor/conquer/config/aosp_audio.mk
 

SwiftAOSP/sw_vendor_lineage

config/common.mk
@@ -107,6 +107,19 @@ PRODUCT_COPY_FILES += \
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/swift/overlay/dictionaries
 
+ifneq ($(HOST_OS),linux)
+ifneq ($(sdclang_already_warned),true)
+$(warning **********************************************)
+$(warning * SDCLANG is not supported on non-linux hosts.)
+$(warning **********************************************)
+sdclang_already_warned := true
+endif
+else
+
+# include definitions for SDCLANG
+include vendor/swift/sdclang/sdclang.mk
+endif
+
 # Include AOSP audio files
 include vendor/swift/config/aosp_audio.mk
 
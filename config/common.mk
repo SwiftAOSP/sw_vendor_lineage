baac9bf2cf4027d29ab86245fb71c3639d3ed432
Jake Weinstein <jake@aospa.co>
2020-12-05 22:32:03 +0530

    vendor: import sdclang configs

--- config/common.mk
+++ config/common.mk
@@ -107,6 +107,19 @@ PRODUCT_COPY_FILES += \
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/swift/overlay/dictionaries
 
+ifneq ($(HOST_OS),linux)
+ifneq ($(sdclang_already_warned),true)
+$(warning **********************************************)
+$(warning * SDCLANG is not supported on non-linux hosts.)
+$(warning **********************************************)
+sdclang_already_warned := true
+endif
+else
+
+# include definitions for SDCLANG
+include vendor/swift/sdclang/sdclang.mk
+endif
+
 # Include AOSP audio files
 include vendor/swift/config/aosp_audio.mk
 
