ConquerOS/vendor_conquer

config: build common required packages
Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

config/common.mk
@@ -114,5 +114,8 @@ include vendor/conquer/config/conquer_audio.mk
 # Include ConquerUI
 include vendor/conquerui/config.mk
 
+# Include Common packages
+include vendor/conquer/config/packages.mk
+
 # Include Version
 include vendor/conquer/config/version.mk

SwiftAOSP/sw_vendor_lineage

config/common.mk
@@ -113,5 +113,8 @@ include vendor/swift/config/aosp_audio.mk
 # Include Swiftify
 include vendor/swiftify/configs/common.mk
 
+# Include swift packages
+include vendor/swift/config/packages.mk
+
 # Include Version
 include vendor/swift/config/version.mk