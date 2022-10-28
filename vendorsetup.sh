ConquerOS/vendor_conquer

vendorsetup.sh: adapt recent manifest changes
Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

vendorsetup.sh
@@ -1,4 +1,4 @@
-export CAF_REVISION_TAG=$(grep "revision" ".repo/manifests/snippets/system.xml" | awk -F '/' '{print $3}' | awk -F '"' '{print $1}')
+export CAF_REVISION_TAG=$(grep "default revision" ".repo/manifests/default.xml" | awk -F '/' '{print $3}' | awk -F '"' '{print $1}')
 
 # SDClang Environment Variables
 export SDCLANG_CONFIG=$(pwd)/vendor/conquer/sdclang/sdclang.json

SwiftAOSP/sw_vendor_lineage

vendorsetup.sh
@@ -1,1 +1,1 @@
-export AOSP_REVISION=$(grep "revision" ".repo/manifests/default.xml" | awk -F '/' '{print $3}' | awk -F '"' '{print $1}')
+export AOSP_REVISION=$(grep "default revision" ".repo/manifests/default.xml" | awk -F '/' '{print $3}' | awk -F '"' '{print $1}')